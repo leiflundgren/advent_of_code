e574eeabaef274e45fde3983a3d1274a5935f9f8
Leif Lundgren <leiflundgren@leiflundgren.com>
2023-12-29 23:50:29 +0100

    test 2.2, raw copy from 2.1

---
+++ 2_2_Cube Conundrum/src/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code
+{
+    public class Program
+    {
+
+        public static Set MasterSet = new Set(12,13, 14);
+
+        public static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory;
+            Console.WriteLine("Path: " + path);
+
+            path = Path.GetFullPath(Path.Combine(path, "..\\..\\..\\..\\input.txt"));
+            Console.WriteLine("Path: " + path);
+
+            string[] lines = File.ReadAllLines(path);
+
+            int sum = 0;
+
+            foreach ( string line in lines)
+            {
+                if  (string.IsNullOrEmpty(line)) continue;
+
+                Game g = Game.Parse(line);
+
+                if ( !g.IsPossible(MasterSet))
+                {
+                    Console.WriteLine(g + " impossible!");
+                }
+                else
+                {
+                    Console.WriteLine(g + " possible");
+                    sum += g.N;
+                }
+            }
+
+            Console.WriteLine($"{sum}"); // 3099
+
+        }
+    }
+}
