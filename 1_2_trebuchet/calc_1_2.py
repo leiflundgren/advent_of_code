1649cb779bebec858d002eab5a6822e47c2e61d1
Leif Lundgren <leiflundgren@leiflundgren.com>
2023-12-29 21:43:01 +0100

    changed to find1st and last

--- 1_2_trebuchet/calc_1_2.py
+++ 1_2_trebuchet/calc_1_2.py
@@ -9,65 +9,40 @@ source_dir = source_path.parent
 
 
 
-digits_dict = {
-    '0': 0,
-    '1': 1,
-    '2': 2,
-    '3': 3 ,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    # '0': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3 ,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-}
-
-def find_digits(s : string):
-    res = []
+def find_1st_digit(s : string):
     
-    if s == "4b\n":
-        bp=17
-
     while len(s) > 0:
-        match = None
         for key in digits_dict:
             l = len(key)
             if s.startswith(key):
-                match = key
-                break
-            
-        if not match is None:
-            res.append(digits_dict[match])
-            s = s[len(match):]
-        else:
-            s = s[1:]
+                return digits_dict[key]
+
+        s = s[1:]
         
-    return res
+    return None
+
+def find_last_digit(s : string):
+    
+    while len(s) > 0:
+        for key in digits_dict:
+            l = len(key)
+            if s.endswith(key):
+                return digits_dict[key]
+
+        s = s[0:-1]
+        
+    return None
+
 
 
 
 sum = 0
 with open( os.path.join(source_dir, "input.txt"), "r") as file:
     for line in file.readlines():
-        digits = find_digits(line)
-        if len(digits) == 0:
-            print("Bad line " + line)
-        elif len(digits) == 1:
-            val = digits[0]
-        else:
-            d1 = digits[0]
-            d2 = digits[-1]
-            val = d1*10 + d2
+        line = line.rstrip('\n\r')
+        d1 = find_1st_digit(line)
+        d2 = find_last_digit(line)
+        val = d1*10 + d2
         
         sum = sum + val
         
