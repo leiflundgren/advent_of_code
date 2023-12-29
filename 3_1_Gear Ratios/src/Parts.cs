76c65faa1f48fbd5ae7d8a26012d3a145c09a60c
Leif Lundgren <leiflundgren@leiflundgren.com>
2023-12-30 00:17:33 +0100

    copied from 2_2

---
+++ 3_1_Gear Ratios/src/Parts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code
+{
+    public class Parts
+    {
+    }
+}
