6de43681f1697bb7b5c626e0801eef0c28ceea34
Leif Lundgren <leiflundgren@leiflundgren.com>
2023-12-31 19:23:46 +0100

    lookup both ways

--- 5_1 Seeds/mapping.py
+++ 5_1 Seeds/mapping.py
@@ -1,26 +1,42 @@
 
+from collections import namedtuple
 
 class Mapping:
     def __init__(self):
+        self.Mapping = namedtuple("Mapping", "dst src len")
         self.mappings = []
         
-    def add_mapping(self, source:int, target:int, len:int):
-        self.mappings.append( (source, target, len))
+    def add_mapping(self, dest:int, source:int, len:int):
+        self.mappings.append( self.Mapping(dest, source, len) )
     
-    def find_mapping(self, src:int):
-        for (s,t,l) in self.mappings:
-            if s <= src and src < s+l:
-                return (s,t,l)
+    def find_src(self, src:int):
+        for m in self.mappings:
+            if m.src <= src and src < m.src+m.len:
+                return m
+        return None
+
+    def find_dst(self, d:int):
+        for m in self.mappings:
+            if m.dst <= d and d < m.dst+m.len:
+                return m
         return None
             
-    def lookup(self, n:int):
-        r = self.find_mapping(n)
-        if r is None:
+    def lookup_src(self, n:int) -> int :
+        m = self.find_src(n)
+        if m is None:
             return n
         
-        (src, target, len) = r
-        offset = n-src
-        return target+offset
+        offset = n-m.src
+        return m.dst+offset
     
 
+    def lookup_dst(self, n:int) -> int :
+        m = self.find_dst(n)
+        if m is None:
+            return n
+        
+        offset = n-m.dst
+        return m.src+offset
     
+
+        
--- 5_1 Seeds/tests.py
+++ 5_1 Seeds/tests.py
@@ -8,10 +8,16 @@ class Tests(unittest.TestCase):
         m = Mapping()
         m.add_mapping(50, 98, 2)
         
-        self.assertEqual(49, m.lookup(49))
-        self.assertEqual(98, m.lookup(50))
-        self.assertEqual(99, m.lookup(51))
-        self.assertEqual(52, m.lookup(52))
+        self.assertEqual(97, m.lookup_src(97))
+        self.assertEqual(50, m.lookup_src(98))
+        self.assertEqual(51, m.lookup_src(99))
+        self.assertEqual(100, m.lookup_src(100))
+
+
+        self.assertEqual(49, m.lookup_dst(49))
+        self.assertEqual(98, m.lookup_dst(50))
+        self.assertEqual(99, m.lookup_dst(51))
+        self.assertEqual(52, m.lookup_dst(52))
         
 
 if __name__ == '__main__':
