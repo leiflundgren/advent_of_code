eedd106df6c8b109722b038fdc1c7096ef6959e9
Leif Lundgren <leiflundgren@leiflundgren.com>
2024-01-05 19:08:47 +0100

    created 5.2#

---
+++ 5_2 Seeds/mapping.py
@@ -0,0 +1,43 @@
+
+from collections import namedtuple
+
+class Mapping:
+    def __init__(self):
+        self.Mapping = namedtuple("Mapping", "dst src len")
+        self.mappings = []
+        
+    def add_mapping(self, dest:int, source:int, len:int):
+        self.mappings.append( self.Mapping(dest, source, len) )
+        return self
+    
+    def find_src(self, src:int):
+        for m in self.mappings:
+            if m.src <= src and src < m.src+m.len:
+                return m
+        return None
+
+    def find_dst(self, d:int):
+        for m in self.mappings:
+            if m.dst <= d and d < m.dst+m.len:
+                return m
+        return None
+            
+    def src_to_dst(self, n:int) -> int :
+        m = self.find_src(n)
+        if m is None:
+            return n
+        
+        offset = n-m.src
+        return m.dst+offset
+    
+
+    def dst_to_src(self, n:int) -> int :
+        m = self.find_dst(n)
+        if m is None:
+            return n
+        
+        offset = n-m.dst
+        return m.src+offset
+    
+
+        
