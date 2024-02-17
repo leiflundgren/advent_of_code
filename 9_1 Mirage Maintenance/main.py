b4cf425e0df5c7b3fac822d68d9b361b29ac9e98
Leif Lundgren <leiflundgren@leiflundgren.com>
2024-02-17 18:15:39 +0100

    success 1743490457

--- 9_1 Mirage Maintenance/main.py
+++ 9_1 Mirage Maintenance/main.py
@@ -1,5 +1,5 @@
 import os
-import desert
+import prog
 from pathlib import Path
 import time
 
@@ -9,13 +9,17 @@ t0 = time.perf_counter()
 input = os.path.join(os.path.dirname(__file__), 'input.txt')
 lines = Path(input).read_text().splitlines()
 
-sc = desert.parse_scenario(iter(lines))
+sum = 0
 
 t1 = time.perf_counter()
 
-steps = sc.walk_to_end()
+
+for l in lines:
+    ints = list(map(lambda s: int(s), l.split(' ')))
+    p = prog.Predictor(ints)
+    sum += p.next_el
 
 t2 = time.perf_counter()
-print(f'steps:{steps}  dt1:{t1-t0}  dt2:{(t2-t1)}') # 20513
+print(f'sum:{sum}  dt1:{t1-t0}  dt2:{(t2-t1)}') # 1743490457
 
 
