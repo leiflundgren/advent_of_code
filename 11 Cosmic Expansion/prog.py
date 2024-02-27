99e27080b93c5e2703c5fea8140bab9545297215
Leif Lundgren <leiflundgren@leiflundgren.com>
2024-02-27 20:36:22 +0100

    first scetches

--- 11 Cosmic Expansion/prog.py
+++ 11 Cosmic Expansion/prog.py
@@ -7,59 +7,50 @@ import functools
 from copy import deepcopy
 
 
-class Map:
+class Springs:
     
     EMPTY = '.'
-    GALAXY = '#'
+    SPRING = '#'
+    UNKNOWN = '?'
 
-    def __init__(self, str:str, empty_cost:int = 2):
-        self.raw_map = str.split()
-        self.height = len(self.raw_map)
-        self.width = len(self.raw_map[0])
+    def __init__(self, str:str):
+        def parse_springs(s:str) -> list[tuple[str, int]]:
+            ls : list[tuple[str, int]] = []
+            for c in s:
+                if len(ls)>0 and ls[-1][0] == c:
+                    ls[-1][1] = 1+ls[-1][1]
+                else:
+                     ls.append((c, 1))
+            pass
+        def parse_arrangement(s:str) -> list[int]:
+            return list(map(lambda s: int(s), s.split(',')))
+        space = str.index(' ')
+        self.springs = str[:space]
+        self.arrangement = parse_arrangement(str[space+1:])
+
+    def reduce(self):
+        changed = True
+        while changed:
+            print(f'{self.springs}')
+            changed = self.reduce_dir(1) or self.reduce_dir(-1)
         
-        self.empty_cost = empty_cost
-        self.cost_vertical = list(map(lambda n: empty_cost if self.is_empty_horizonal(n) else 1, range(self.height)))
-        self.cost_horizontal = list(map(lambda n: empty_cost if self.is_empty_vertical(n) else 1, range(self.width)))
+    def reduce_dir(self, dir:int):
+        EMPTY = Springs.EMPTY
+        SPRING = Springs.SPRING 
+        UNKNOWN = Springs.UNKNOWN 
+
+        changed = True
+        while True:
+            c = self.springs[dir]
+            if c == EMPTY:
+                del self.springs[dir]
+                print(f'{self.springs}')
+                continue
         
-        self.galaxies = []
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.raw_map[y][x] == Map.GALAXY:
-                    self.galaxies.append((x, y))
-        self.pairs = []
-            
-    def is_empty_horizonal(self, n:int) -> bool:
-        return all(map(lambda c: c == Map.EMPTY, self.raw_map[n]))
-    def is_empty_vertical(self, n:int) -> bool:
-        return all(map(lambda line: line[n] == Map.EMPTY, self.raw_map))
-    
-    def get(self, coord: tuple[int,int]) -> str:
-        (x,y) = coord
-        return self.raw_map[y][x]
-    
-    def cost(self, coord1 : tuple[int,int], coord2 : tuple[int,int]) -> int:
-        (x1,y1) = coord1
-        (x2,y2) = coord2
+            if c == SPRING:
+                del self.springs[dir]                
+                print(f'{self.springs}')
+                continue
 
-        # length is same if mirrored
-        if x1 > x2: (x1, x2) = (x2, x1)
-        if y1 > y2: (y1, y2) = (y2, y1)
 
-        return \
-            sum(map(lambda x:self.cost_horizontal[x], range(x1, x2))) \
-            + sum(map(lambda y:self.cost_vertical[y], range(y1, y2)))
-    
-    def get_galaxie_pairs(self):
-        if len(self.pairs) == 0:
-            for g1 in range(len(self.galaxies)-1):
-                for g2 in range(g1+1, len(self.galaxies)):
-                    if g1 != g2:
-                        self.pairs.append((self.galaxies[g1], self.galaxies[g2]))
-        return self.pairs
-                    
-
-    def sum_dist_pairs(self):
-        pairs = self.get_galaxie_pairs()
-        return sum(map( lambda pair: self.cost(*pair), pairs))
-            
-
+            changed = False
