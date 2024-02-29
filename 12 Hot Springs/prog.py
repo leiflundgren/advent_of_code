61f857bbe39ff6ac35e229342f0899915826e695
Leif Lundgren <leiflundgren@leiflundgren.com>
2024-02-29 21:58:08 +0100

    refactored reducing

--- 12 Hot Springs/prog.py
+++ 12 Hot Springs/prog.py
@@ -1,6 +1,6 @@
 import tools
 from enum import Enum
-from typing import Iterator, Self
+from typing import Iterable, Iterator, Self
 import re
 from itertools import tee
 import functools
@@ -13,58 +13,63 @@ class Springs:
     SPRING = '#'
     UNKNOWN = '?'
 
-    def __init__(self, springs:list[tuple[str, int]], arrangement:list[int]):
+    def __init__(self, springs:list[tuple[str, int]], arrangment:list[int]):
         self.springs = springs
-        self.arrangement = arrangement
+        self.arrangment = arrangment
 
     def __str__(self):
-        return f'{self.springs}  {self.arrangement}'
+        return Springs.makestring(self.springs, self.arrangment)
+    
+    @staticmethod
+    def makestring(springs:list[tuple[str, int]], arrangment:list[int]) -> str:
+        return f'{springs}     {arrangment}'
         
     def reduce(self):
-        while True:
-            print(self)
-            
-            nxt_front = self.reduce_dir(0)
-            nxt_back = (self if nxt_front is None else nxt_front).reduce_dir(-1)
-            
-            if not nxt_back is None:
-                return nxt_back.reduce()
-            elif not nxt_back is None:
-                return nxt_front.reduce()
-            else:
-                return self
+        springs = self.springs
+        arrangment = self.arrangment
+        print(self)
         
-    def reduce_dir(self, dir:int) -> (bool, Self):
-        def copy_except(ls:list, dir:int) -> list:
-            return ls[1:] if dir == 0 else ls[:-1]
+        reduced_fwd = reduced_back = True
         
+        while reduced_fwd or reduced_back:
 
+            (reduced_fwd, pop_springs, pop_arragement) = Springs.can_reduce(springs[0][0], springs[0][1], arrangment[0])
+            if reduced_fwd:
+                if pop_springs: springs.pop(0)
+                if pop_arragement: arrangment.pop(0)
+                print(Springs.makestring(springs, arrangment))
+               
+            if len(springs) > 0 and len(arrangment) > 0:
+                (reduced_back, pop_springs, pop_arragement) = Springs.can_reduce(springs[-1][0], springs[-1][1], arrangment[-1])
+                if reduced_back:
+                    if pop_springs: springs.pop(-1)
+                    if pop_arragement: arrangment.pop(-1)
+                    print(Springs.makestring(springs, arrangment))
+
+        return Springs(springs, arrangment)
+    
+
+    # returns (can_reduce, consume_spring, consume_arr)
+    @staticmethod
+    def can_reduce(spring:str, cnt:int, arr:int) -> (bool, bool, bool):
+        
         EMPTY = Springs.EMPTY
         SPRING = Springs.SPRING 
         UNKNOWN = Springs.UNKNOWN 
-        
-        if len(self.springs) == 0:
-            return None
-                    
-        rev = -1 if dir == 0 else 0
-        (c, cnt) = self.springs[dir]
-        if c == EMPTY:
-            changed = Springs(copy_except(self.springs, dir), self.arrangement)
-            print(changed)
-            return changed
-        
-        arr = self.arrangement[dir]
-        if c == SPRING:
-            changed = Springs(copy_except(self.springs, dir), copy_except(self.arrangement, dir))
-            print(changed)
-            return changed
-        
-        if c == UNKNOWN and cnt == arr: 
-            changed = Springs(copy_except(self.springs, dir), copy_except(self.arrangement, dir))
-            print(changed)
-            return changed
+     
+        if spring == EMPTY:
+            # use we have not called next of arrangment yet
+            return (True, True, False)
 
-        return None
+        if spring == SPRING:
+            return (True, True, True)
+        
+        # if spring == UNKNOWN and cnt == arr: 
+        #     return (True, True, True)
+        
+        return (False, False, False)
+
+                            
     
 def parse_springs(str:str):
     def parse_springs(s:str) -> list[tuple[str, int]]:
@@ -81,11 +86,12 @@ def parse_springs(str:str):
                     ls.append((c, 1))
         return ls
         
-    def parse_arrangement(s:str) -> list[int]:
+    def parse_arrangment(s:str) -> list[int]:
         return list(map(lambda s: int(s), s.split(',')))
         
+    print('Parse: ', str)
     space = str.index(' ')
     springs = parse_springs(str[:space])
-    arrangement = parse_arrangement(str[space+1:])
-    return Springs(springs, arrangement)
+    arrangment = parse_arrangment(str[space+1:])
+    return Springs(springs, arrangment)
 
--- 12 Hot Springs/tests.py
+++ 12 Hot Springs/tests.py
@@ -1,3 +1,4 @@
+from math import comb
 import unittest
 import prog
 from prog import Springs
@@ -19,12 +20,12 @@ def scenario1(): return \
 '.###....##.#'])
 
 def scenario2(): return [
-    (prog.parse_springs('???.### 1,1,3'), 1),
-    (prog.parse_springs('.??..??...?##. 1,1,3'), 4),
-    (prog.parse_springs('?#?#?#?#?#?#?#? 1,3,1,6'), 1),
-    (prog.parse_springs('????.#...#... 4,1,1'), 1),
-    (prog.parse_springs('????.######..#####. 1,6,5'), 4),
-    (prog.parse_springs('?###???????? 3,2,1'), 10),
+    ('???.### 1,1,3', 1),
+    ('.??..??...?##. 1,1,3', 4),
+    ('?#?#?#?#?#?#?#? 1,3,1,6', 1),
+    ('????.#...#... 4,1,1', 1),
+    ('????.######..#####. 1,6,5', 4),
+    ('?###???????? 3,2,1', 10),
 ]
 
 
@@ -34,14 +35,15 @@ class Tests(unittest.TestCase):
         (scen, solution) = scenario1()
         
         springs = prog.parse_springs(scen)
-        self.assertEqual([3,2,1], springs.arrangement)
+        self.assertEqual([3,2,1], springs.arrangment)
         self.assertEqual([('?', 1), ('#', 3), ('?', 8)], springs.springs)
 
 
     def test_scenario2(self):
         scenarios = scenario2()
-        for (n, (springs, combinations)) in zip(range(len(scenarios)), scenarios):
-            print(f'Reduce scen {n}')
+        for (n, (str_springs, combinations)) in zip(range(len(scenarios)), scenarios):
+            print(f'\nReduce scen {n}: {str_springs}  arr:{combinations}')
+            springs = prog.parse_springs(str_springs)
             springs.reduce()
 
 if __name__ == '__main__':
