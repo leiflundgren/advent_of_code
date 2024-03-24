bcafc4865e962bac5389e851027f2e180981e1de
Leif Lundgren <leiflundgren@leiflundgren.com>
2024-03-24 20:25:30 +0100

    passes trivial tests

--- 13 Point of Incidence/prog.py
+++ 13 Point of Incidence/prog.py
@@ -6,13 +6,38 @@ class Matrix:
     def __init__(self, src : list[str]):
         self.data = src
         
+    # get 1-indexed
+    def getLine(self, i : int) -> str:
+        return self.data[i-1]
 
+    def __len__(self) -> int:
+        return len(self.data)
     def __str__(self) -> str:
         return '\n'.join(self.data)
     
     def rotate(self) -> Self:
-        y = list(map(lambda i: ''.join([ self.data[j][i] for j in range(len(self.data)) ]), range(len(self.data[0]))))
+        y = list(map(lambda i: ''.join([ self.data[j][i] for j in range(len(self)) ]), range(len(self.data[0]))))
         return Matrix(y)
                 
-
-
+    def find_equal_lines(self) -> list[int]:
+        res = []
+        for i in range(1, len(self)):
+            if self.getLine(i) == self.getLine(i+1):
+                res.append(i)
+        return res
+    
+    def check_if_mirror(self, line:int) -> bool:
+        (p1, p2) = (line, line+1)
+        while p1 > 0 and p2 <= len(self):
+            l1 = self.getLine(p1)
+            l2 = self.getLine(p2)
+            if l1 != l2: 
+                return False
+            
+            p1 = p1-1
+            p2 = p2+1
+        return True
+    
+    def find_mirrors(self) -> list[int]:
+        return list(filter(lambda n: self.check_if_mirror(n), self.find_equal_lines()))
+            
--- 13 Point of Incidence/tests.py
+++ 13 Point of Incidence/tests.py
@@ -26,15 +26,29 @@ pattern2 = \
 class Tests(unittest.TestCase):
     def test_parse(self):
         map1 = Matrix(pattern1)
+        print()
         print(map1)
+        self.assertEqual([3], map1.find_equal_lines())
+        self.assertEqual([], map1.find_mirrors())
+        
         rot1 = map1.rotate()
         print()
         print(rot1)
         
-
+        self.assertEqual([5], rot1.find_equal_lines())
+        self.assertEqual([5], rot1.find_mirrors())
 
         map2 = Matrix(pattern2)
+        print()
+        print(map2)
+        self.assertEqual([4], map2.find_equal_lines())
+        self.assertEqual([4], map2.find_mirrors())
+
         rot2 = map2.rotate()
+        print()
+        print(rot2)
+        self.assertEqual([3, 7], rot2.find_equal_lines())
+        self.assertEqual([], rot2.find_mirrors())
         
 if __name__ == '__main__':
     unittest.main()
