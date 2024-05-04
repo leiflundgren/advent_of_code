8cee5b9a2d701f68f7bc8385b4e2aa8d07afbb40
Leif Lundgren <leif.lundgren@mitel.com>
2024-05-04 18:08:42 +0200

    first attempt at rust

---
+++ 17 Clumsy Crucible/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+    let _g : i32 = add(1, 2);
+    let _b : i32 = bad_add(1, 2);
+}
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// This is a really bad adding function, its purpose is to fail in this
+// example.
+#[allow(dead_code)]
+fn bad_add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
