ce29e6f7075372fb00aa2148510433e57b4e035f
Leif Lundgren <leif.lundgren@mitel.com>
2024-08-22 23:18:47 +0200

    Direction finding works

--- 17 Clumsy Crucible/src/path.rs
+++ 17 Clumsy Crucible/src/path.rs
@@ -1,8 +1,8 @@
-use std::{collections::VecDeque, mem};
+use std::{collections::VecDeque, fmt::{self, Debug}, mem};
 
 use crate::map;
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(PartialEq, Copy, Clone, Debug)]
 pub enum Direction {
     North,
     East,
@@ -10,24 +10,51 @@ pub enum Direction {
     West,
 }
 
+#[derive(Debug)]
 pub struct PathNode {
     x: i32,
     y: i32,
     cost: i32,
 }
 
-pub fn get_direction(n1:&PathNode, n2:&PathNode) -> Direction {
-    if n1.x == n2.x {
-        if n1.y == n2.y + 1 { Direction::South }
-        else if n1.y == n2.y - 1 { Direction::North }
-        else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+impl PathNode{
+    pub fn new(x: i32,y: i32, cost: i32,)  -> Self {
+        PathNode { x : x, y : y, cost : cost }
     }
-    else if n1.y == n2.y {
-        if n1.x == n2.x + 1 { Direction::East }
-        else if n1.x == n2.x - 1 { Direction::West }
-        else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+}
+
+// Implement `Display` for `PathNode`.
+impl fmt::Display for PathNode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {}): {}", self.x, self.y, self.cost)
     }
-    else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+}
+
+
+pub fn get_direction(o_n1:Option<&PathNode>, o_n2:Option<&PathNode>) -> Direction {
+    match o_n1 {
+        None => Direction::South,
+        Some(n1) => { 
+            match o_n2 {
+                None => panic!("2nd node is None. Cannot deduce direction"),
+                Some(n2) => 
+                { 
+                    if n1.x == n2.x {
+                        if n1.y + 1 == n2.y { Direction::South }
+                        else if n1.y == n2.y + 1 { Direction::North }
+                        else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+                    }
+                    else if n1.y == n2.y {
+                        if n1.x + 1 == n2.x { Direction::East }
+                        else if n1.x == n2.x + 1 { Direction::West }
+                        else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+                    }
+                    else { panic!("Invalid direction n1:{x1},{y1} n2:{x2},{y2}", x1=n1.x, y1=n1.y, x2=n2.x, y2=n2.y) }
+                }
+            }
+        }
+    }
 }
 
 pub struct Path {
@@ -41,31 +68,41 @@ impl Path {
 
     pub fn get_direction(& self, depth:usize) -> Direction {
         
-        if depth == 0 { 
+        let len : usize = self.head.len();
+        
+        if depth > len { 
             Direction::South 
         }
-        else if depth <= self.head.len() {
-            let n1 = self.head.get(depth-1).unwrap();
-            let n2 = self.head.get(depth).unwrap();
+        else if depth < len {
+            let n2 = self.head.get(depth);
+            let n1 = self.head.get(depth+1);
             get_direction(n1, n2)
         }
         else { 
-            panic!("Attempt to get depth {depth} when list.len is {len}", depth=depth, len=self.head.len())
+            panic!("Attempt to get depth {depth} when list.len is {len}")
         }        
+    }
+
+    pub fn add_step(&mut self, x:i32, y:i32, cost:i32) {
+        assert!( match self.head.front() {
+            None => true,
+            Some(front) => (front.x - x).abs() == 1 || (front.y - y).abs() == 1
+        });
+        self.head.push_front(PathNode::new(x, y, cost));
     }
 
     // If we have travelled same direction last `depth` steps, return that direction. None otherwise.
     pub fn same_direction_last(& self, depth:usize) -> Option<Direction> {
         if depth <1 { return  None; }
-        if self.head.len() < depth { return None; }
+        if self.head.len() <= depth { return None; }
 
-        let mut n0 : &PathNode = self.head.get(0).unwrap();
-        let mut n1 : &PathNode = self.head.get(1).unwrap();
+        let mut n0 : Option<&PathNode> = self.head.get(0);
+        let mut n1 : Option<&PathNode> = self.head.get(1);
 
         let mut d1 = get_direction(n0, n1);
         
         for i in 2..depth {            
-            let n2 = self.head.get(i).unwrap();
+            let n2: Option<&PathNode> = self.head.get(i);
             let d2 = get_direction(n1, n2);
             if d1 != d2 { return None; } // changed dir within depth
 
@@ -109,14 +146,43 @@ impl Path {
 
 #[cfg(test)]
 mod test {
+    use std::f32::consts::E;
+
     use super::*;
 
     #[test]
     fn basics() {
-        // let mut list = List::new();
+        let mut p = Path::new();
 
-        // // Check empty list behaves right
-        // assert_eq!(list.pop(), None);
+        p.add_step(0, 0,10);
+
+        assert_eq!(p.get_direction(0), Direction::South);
+
+        p.add_step(0, 1,20);
+
+        assert_eq!(p.get_direction(0), Direction::South);
+        assert_eq!(p.get_direction(1), Direction::South);
+
+        p.add_step(1, 1,20);
+
+        assert_eq!(p.get_direction(0), Direction::East);
+        assert_eq!(p.get_direction(1), Direction::South);
+        assert_eq!(p.get_direction(2), Direction::South);
+
+        p.add_step(1, 0,20);
+
+        assert_eq!(p.get_direction(0), Direction::North);
+        assert_eq!(p.get_direction(1), Direction::East);
+        assert_eq!(p.get_direction(2), Direction::South);
+        assert_eq!(p.get_direction(3), Direction::South);
+
+        p.add_step(0, 0,20);
+
+        assert_eq!(p.get_direction(0), Direction::West);
+        assert_eq!(p.get_direction(1), Direction::North);
+        assert_eq!(p.get_direction(2), Direction::East);
+        assert_eq!(p.get_direction(3), Direction::South);
+        assert_eq!(p.get_direction(4), Direction::South);
 
         // // Populate list
         // list.push(1);
