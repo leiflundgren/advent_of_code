8f1dc6eb5478c03970282c1ad1c8310bbb6fc439
Leif Lundgren <leif.lundgren@mitel.com>
2024-12-07 23:48:29 +0100

    PAssing test 3.2

--- 2024/3 Mull It Over/tests.py
+++ 2024/3 Mull It Over/tests.py
@@ -17,19 +17,28 @@ class Tests(unittest.TestCase):
         actual = prog.parse_muls_pairs(pattern1)
         self.assertEqual(expected_results, actual, 'Test 3.1')
 
-    # def test_dot2(self):
-    #     raise
-    #     expected_results = [True, False, False, True, True, True]
-    #     self.general(expected_results, prog.is_safe_if_remove_one)
-        
-    #     actual0 = prog.is_safe([1,3,2,4,5])
-    #     actual1 = prog.is_safe_if_remove_one([1,3,2,4,5])
-    #     actual2 = prog.is_safe([1,2,4,5])
-    #     actual3 = prog.is_safe([1,3,4,5])
-    #     self.assertEqual(False, actual0)
-    #     self.assertEqual(True, actual1)
-    #     self.assertEqual(True, actual2)
-    #     self.assertEqual(True, actual3)
+    def test_dot2(self):
+        pattern1 = '''xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'''
+        # xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+        expected_results = ['mul(2,4)','don\'t()', 'mul(5,5)', 'mul(11,8)', 'do()', 'mul(8,5)']
+        actual = prog.parse_inputs(pattern1)
+        print(actual)
+        self.assertEqual(expected_results, actual, 'Test 3.2a')
+
+        expected_results = ['mul(2,4)', 'mul(8,5)']
+        actual = prog.filter_inputs(actual)
+        print(actual)
+
+        self.assertEqual(expected_results, actual, 'Test 3.2b')
+
+        inputs = [ prog.parse_muls_pairs(inp)[0] for inp in actual ]
+        print(inputs)
+
+        actual_sum = prog.multiply_inputs(inputs)
+        self.assertEqual(48, actual_sum)
+
+
 
    
 
