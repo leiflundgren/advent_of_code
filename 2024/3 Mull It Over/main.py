80eaadd24da121535027512f1338df42d137e761
Leif Lundgren <leif.lundgren@mitel.com>
2024-12-07 22:54:33 +0100

    3.1  correct

--- 2024/3 Mull It Over/main.py
+++ 2024/3 Mull It Over/main.py
@@ -9,22 +9,16 @@ import tools
 def main():
 
     input_txt = os.path.join(os.path.dirname(__file__), 'input.txt')
-    input_str = list(Path(input_txt).read_text().splitlines())
-    input = list(map(prog.parse_line, input_str))
-    sum_1 = 0; sum_2 = 0
+    input_str = ''.join(list(Path(input_txt).read_text().splitlines()))
+    input = prog.parse_muls_pairs(input_str)
+    sum_1 = 0
+    sum_2 = 0
     
     # Part 1
-    for (n, levels) in zip(tools.natural_numbers(), input):
-        safe0 = prog.is_safe(levels)
-        safe1 = prog.is_safe_if_remove_one(levels)
-        if safe0:
-            sum_1 += 1
-        if safe1:
-            sum_2 += 1
-        if not safe0:
-            print(f'{levels} : {safe0}, {safe1}')
+    for (n, (x, y)) in zip(tools.natural_numbers(), input):
+        sum_1 += x*y
 
-    print(f'sum1:{sum_1}  sum2:{sum_2}')    ## sum1:242  sum2:311
+    print(f'sum1:{sum_1}  sum2:{sum_2}')    ## sum1:191183308  sum2:311
 
 if __name__ == '__main__':
     main()
