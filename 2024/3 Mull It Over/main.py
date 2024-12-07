6b82ed5f40b402967f8df0cdb034a0f04cca060f
Leif Lundgren <leif.lundgren@mitel.com>
2024-12-07 23:51:12 +0100

    solved 4.2

--- 2024/3 Mull It Over/main.py
+++ 2024/3 Mull It Over/main.py
@@ -17,8 +17,13 @@ def main():
     # Part 1
     for (n, (x, y)) in zip(tools.natural_numbers(), input):
         sum_1 += x*y
+    
+    actual = prog.parse_inputs(input_str)
+    actual = prog.filter_inputs(actual)
+    inputs = [ prog.parse_muls_pairs(inp)[0] for inp in actual ]
+    sum_2 = prog.multiply_inputs(inputs)
 
-    print(f'sum1:{sum_1}  sum2:{sum_2}')    ## sum1:191183308  sum2:311
+    print(f'sum1:{sum_1}  sum2:{sum_2}')    ## sum1:191183308  sum2:92082041
 
 if __name__ == '__main__':
     main()
