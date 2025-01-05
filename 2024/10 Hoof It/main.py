75f4e727e946017975a67e95ea194b29db454081
Leif Lundgren <leif.lundgren@mitel.com>
2025-01-05 23:36:29 +0100

    fixed 10.2 by accident

--- 2024/10 Hoof It/main.py
+++ 2024/10 Hoof It/main.py
@@ -2,6 +2,8 @@ import datetime
 import os
 from pathlib import Path
 import time
+from typing import Set
+from map import Point
 import prog
 from text_map import TextMap
 import tools
@@ -19,51 +21,40 @@ def prBlack(skk): print("\033[98m {}\033[00m" .format(skk), end='')
 # entry point
 def main():
 
+    sum_1 = sum_2 = 0
+
     input_txt = os.path.join(os.path.dirname(__file__), 'input.txt')
     input_str = Path(input_txt).read_text()
 
     t0 = time.perf_counter()
 
-    m = TextMap.parse_text(input_str)   
-    v = prog.Guard.find_guard_pos(m)
+    m = TextMap.parse_text(input_str)
 
-    g = prog.Guard(v, m)
-    g.walk_path()
+    path = prog.Path(m)
 
-    # def color_select(x: int, y:int, d:Direction) -> str:
-    #     return prRed if Point(x,y) in g.history else prBlack
+    for start in path.find_start_pos():
+        paths = path.walk_path(start)
 
-    #m.print(color_select)
+        endp : Set[Point] = set()
+        for p in paths:
+            endp.add(p[-1])
 
-    sum_1 = g.count_unique_positions()
+        score = len(endp)
+        rating = len(paths)
+            
+        print(f'start:{start} score: {score} rating:{rating}')
+        # for p in paths:
+        #     print(Path.to_string_map(m, p))
+            
+        sum_1 += score
+        sum_2 += rating
+    
 
     dt1 = time.perf_counter() - t0
     print(f'sum1:{sum_1}')  
     print(f'dt1:{dt1}') 
 
-    # g.print_path(prBlack, prRed)
-    path1 = g.history
-
     t1 = time.perf_counter()
-
-    mods = []
-    path_len = len(set([v.pos for v in g.history]))
-    for ((attempt, p), n) in zip(g.modify_path_to_create_loops_yield(), tools.natural_numbers()):
-        mods.append(p)
-        print(f'{n} obst={p} attempt={attempt} mods={len(mods)} ')
-        dt = time.perf_counter() - t1
-        t_per_attempt = dt / attempt
-        est_total_time = t_per_attempt * path_len
-        est_time_left = (int)(t_per_attempt * (path_len-attempt))
-
-
-# get '14:39:57':
-# est_done.strftime('%H:%M:%S')
-        
-        print(f'time/attempt={t_per_attempt:.4f} est_total={est_total_time:.2f} est_time_left={est_time_left}')
-
-
-    sum_2 = len(mods)
 
     dt2 = time.perf_counter() - t1
 
