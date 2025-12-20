802b1ed2d1b7997f2f3c55791fdbf991b5e69732
Leif Lundgren <leif.lundgren@mitel.com>
2025-12-20 20:56:56 +0100

    Passing B tests

--- 2025/03_lobby/prog.go
+++ 2025/03_lobby/prog.go
@@ -25,8 +25,37 @@ func indexesOfChar(s string, ch rune) []int {
 	return idxs
 }
 
+var powerOfTen []int
+
+// IntPow calculates n to the mth power. Since the result is an int, it is assumed that m is a positive power
+func IntPow(n, m int) int {
+	if m == 0 {
+		return 1
+	}
+
+	if m == 1 {
+		return n
+	}
+
+	result := n
+	for i := 2; i <= m; i++ {
+		result *= n
+	}
+	return result
+}
+
 func find_max_joltage(strBatteries string, isA bool) int {
 
+	if !isA {
+
+		length := 12
+		powerOfTen = make([]int, length+1)
+		powerOfTen[0] = 1
+		for i := 1; i <= length; i++ {
+			powerOfTen[i] = powerOfTen[i-1] * 10
+		}
+		return find_max_joltage_cached(strBatteries, 0, length, make(map[[2]int]int))
+	}
 	max := -1
 
 	maxIdx := find_max_index(strBatteries, 0, len(strBatteries)-1)
@@ -46,4 +75,45 @@ func find_max_joltage(strBatteries string, isA bool) int {
 	}
 
 	return max
+}
+
+func find_max_joltage_cached(strBatteries string, start int, length int, cache map[[2]int]int) int {
+
+	if length == 0 {
+		return 0
+	}
+
+	batlen := len(strBatteries)
+	if length > 12 {
+		panic("invalid argument: length > 12")
+	}
+
+	key := [2]int{start, length}
+	if v, ok := cache[key]; ok {
+		return v
+	}
+
+	var v int
+
+	if batlen-start == length {
+		v, _ = strconv.Atoi(strBatteries[start:])
+	} else {
+		// include first
+		va, _ := strconv.Atoi(string(strBatteries[start]))
+		pow := powerOfTen[length-1]
+		vb := find_max_joltage_cached(strBatteries, start+1, length-1, cache)
+		v1 := va*pow + vb
+
+		// don't include first
+		v2 := find_max_joltage_cached(strBatteries, start+1, length, cache)
+
+		if v1 > v2 {
+			v = v1
+		} else {
+			v = v2
+		}
+	}
+
+	cache[key] = v
+	return v
 }
--- 2025/03_lobby/prog_test.go
+++ 2025/03_lobby/prog_test.go
@@ -5,9 +5,15 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 )
 
-var test_input []string
+var test_input = []string{
+	"987654321111111",
+	"811111111111119",
+	"234234234234278",
+	"818181911112111",
+}
 
 func test_range(t *testing.T, i int, testName string, testInput []string, expected []int) int {
 	maxj := find_max_joltage(testInput[i], testName == "A")
@@ -18,7 +24,7 @@ func test_range(t *testing.T, i int, testName string, testInput []string, expect
 			return -1
 		}
 	}
-	fmt.Printf("Test %s-%d: max joltage: %d\n", testName, i, maxj)
+	// fmt.Printf("Test %s-%d: max joltage: %d\n", testName, i, maxj)
 	return maxj
 }
 
@@ -37,20 +43,24 @@ func Test_A(t *testing.T) {
 	}
 }
 
-// func Test_B(t *testing.T) {
+func Test_B(t *testing.T) {
 
-// 	sum := 0
-// 	for i := 0; i < len(id_ranges); i++ {
-// 		sum += test_range(t, i, "B")
-// 	}
+	expected := []int{987654321111, 811111111119, 434234234278, 888911112111}
 
-// 	expected_sum := 4174379265
-// 	if sum != expected_sum {
-// 		t.Errorf("Total sum of invalid IDs: got %d, want %d", sum, expected_sum)
-// 	}
-// }
+	sum := 0
+	for i := 0; i < len(test_input); i++ {
+		sum += test_range(t, i, "B", test_input, expected)
+	}
+
+	expected_sum := 3121910778619
+	if sum != expected_sum {
+		t.Errorf("Total sum of invalid IDs: got %d, want %d", sum, expected_sum)
+	}
+}
 
 func TestInput(t *testing.T) {
+
+	t0 := time.Now()
 
 	// Stop simple tests
 	content, _ := os.ReadFile("input.txt")
@@ -61,24 +71,19 @@ func TestInput(t *testing.T) {
 		sum += test_range(t, i, "A", lines, nil)
 	}
 
-	fmt.Printf("A: Total sum of invalid IDs: %d\n", sum) // 16854
+	fmt.Printf("A: Total sum of invalid IDs: %d took %s\n", sum, time.Since(t0)) // 16854
 
-	// sum = 0
+	t0 = time.Now()
+	sum = 0
 	// for i := 0; i < len(id_ranges); i++ {
 	// 	sum += test_range(t, i, "B")
 	// }
 
-	// fmt.Printf("B: Total sum of invalid IDs: %d\n", sum) // 44143124633
+	fmt.Printf("B: Total sum of invalid IDs: %d took %s\n", sum, time.Since(t0)) // 16854
+
 }
 
 func TestMain(m *testing.M) {
-
-	test_input = []string{
-		"987654321111111",
-		"811111111111119",
-		"234234234234278",
-		"818181911112111",
-	}
 
 	exitVal := m.Run()
 
