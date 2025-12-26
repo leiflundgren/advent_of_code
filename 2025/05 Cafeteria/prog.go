4f43b8c0bbaf7366dec0603ed9572eb6c87b26d9
Leif Lundgren <leif.lundgren@mitel.com>
2025-12-26 20:58:54 +0100

    passing whole day 5

--- 2025/05 Cafeteria/prog.go
+++ 2025/05 Cafeteria/prog.go
@@ -2,6 +2,7 @@ package day05
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -10,10 +11,28 @@ type Range struct {
 	End   int
 }
 
+// Implementing the String method from fmt.Stringer interface
+func (r *Range) String() string {
+	return fmt.Sprintf("[%d -- %d]", r.Start, r.End)
+}
+
+func make_range(start int, end int) Range {
+	return Range{Start: start, End: end}
+}
+func sort_ranges(a Range, b Range) int {
+	if a.Start != b.Start {
+		return a.Start - b.Start
+	} else {
+		return a.End - b.End
+	}
+}
+
 type Input struct {
 	Ranges []Range
 	IDs    []int
 }
+
+var Zero_Range = Range{Start: 0, End: 0}
 
 func parse_IDs(input string) Range {
 
@@ -75,4 +94,55 @@ func (inp *Input) find_fresh() []int {
 		}
 	}
 	return fresh_ids
+}
+
+func do_intersect(r1 Range, r2 Range) bool {
+	return r1.Start <= r2.End && r2.Start <= r1.End || r2.Start <= r1.End && r1.Start <= r2.End
+}
+
+func merge_ranges(r1 Range, r2 Range) Range {
+	//fmt.Printf("Merging %v and %v\n", r1, r2)
+	return Range{Start: min(r1.Start, r2.Start), End: max(r1.End, r2.End)}
+}
+
+func merge_intersecting_ranges(ranges []Range) []Range {
+	if len(ranges) <= 1 {
+		return ranges
+	}
+
+	cpy := make([]Range, len(ranges))
+	copy(cpy, ranges)
+	slices.SortFunc(cpy, sort_ranges)
+
+	ret := make([]Range, 0, len(ranges))
+	r1 := cpy[0]
+	for i, j := 0, 1; ; {
+
+		if j == len(cpy) { // end
+			ret = append(ret, r1)
+			break
+		}
+
+		r2 := cpy[j]
+
+		if !do_intersect(r1, r2) {
+			ret = append(ret, r1)
+			i = j
+			j = i + 1
+			r1 = r2
+			continue
+		}
+
+		r1 = merge_ranges(r1, r2)
+		j++
+	}
+	return ret
+}
+
+func count_ranges(ranges []Range) int {
+	count := 0
+	for _, r := range ranges {
+		count += r.End - r.Start + 1
+	}
+	return count
 }
--- 2025/05 Cafeteria/prog_test.go
+++ 2025/05 Cafeteria/prog_test.go
@@ -49,41 +49,50 @@ func Test_A(t *testing.T) {
 	}
 }
 
+func Test_B(t *testing.T) {
+
+	cpy := make([]Range, len(test_input.Ranges))
+	copy(cpy, test_input.Ranges)
+	slices.SortFunc(cpy, sort_ranges)
+	sorted := make([]Range, 4)
+	sorted[0] = make_range(3, 5)
+	sorted[1] = make_range(10, 14)
+	sorted[2] = make_range(12, 18)
+	sorted[3] = make_range(16, 20)
+
+	if !slices.Equal(cpy, sorted) {
+		t.Errorf("FAIL: Expected sorted ranges %v, got %v", sorted, cpy)
+	}
+
+	actual := merge_intersecting_ranges(test_input.Ranges)
+	if len(actual) != 2 {
+		t.Errorf("FAIL: Expected 2 ranges,  got %d", len(test_input.IDs))
+	}
+
+	cnt := count_ranges(actual)
+	if cnt != 14 {
+		t.Errorf("FAIL: Expected total count of merged ranges to be 14, got %d", cnt)
+	}
+}
+
 func TestInput(t *testing.T) {
 
 	t0 := time.Now()
 
 	// Stop simple tests
 	content, _ := os.ReadFile("input.txt")
-	input := parse_input(strings.Split(string(content), "\n"))
+	test_input := parse_input(strings.Split(string(content), "\n"))
 
-	actual := input.find_fresh()
+	actual := test_input.find_fresh()
 
 	fmt.Printf("A: Total sum of fresh: %d took %s\n", len(actual), time.Since(t0)) // 1363
 
-	// t0 = time.Now()
+	t0 = time.Now()
 
-	// sum := 0
-	// for i := 0; ; i++ {
+	ranges := merge_intersecting_ranges(test_input.Ranges)
 
-	// 	fmt.Printf("Iteration %d\n", i)
-	// 	for _, line := range expanded {
-	// 		fmt.Println(line)
-	// 	}
-
-	// 	accessible_rolls := find_rolls_with_n_free_edges(expanded, 5)
-
-	// 	if len(accessible_rolls) == 0 {
-	// 		break
-	// 	}
-
-	// 	sum += len(accessible_rolls)
-	// 	fmt.Printf("iter %d  Found %d accessible rolls\n", i, len(accessible_rolls))
-
-	// 	setIndices(accessible_rolls, '.', expanded)
-	// }
-
-	// fmt.Printf("B: Total sum of accessible rolls: %d took %s\n", sum, time.Since(t0)) // 8184
+	cnt := count_ranges(ranges)
+	fmt.Printf("B: Total sum of fresh ingredients: %d took %s\n", cnt, time.Since(t0)) // 8184
 
 }
 
